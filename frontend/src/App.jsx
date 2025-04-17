a5655b10992c8fee0f5e46c4d3ca7198c661b528
Subhin MS <subhinsubramanian@gmail.com>
2025-04-17 16:02:56 +0530

    Initial MERN file setup completed, installed React, Tailwind, and Flowbite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button, Modal, ModalBody } from 'flowbite-react'
+import React from 'react'
+
+function App() {
+  return (
+    <div className=''>
+      <Button className="bg-gradient-to-br from-green-400 to-blue-600 text-white hover:bg-gradient-to-bl focus:ring-green-200 dark:focus:ring-green-800">
+      Green to Blue</Button>  
+    </div>
+  )
+}
+
+export default App
