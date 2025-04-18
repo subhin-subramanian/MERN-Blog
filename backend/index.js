7f7d3e1a5b147552b1709e54e43a535721d1e4c1
Subhin MS <subhinsubramanian@gmail.com>
2025-04-18 11:02:44 +0530

    Database Connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
-
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
 const app = express();
 
+dotenv.config();
+
 app.listen(3000,()=>{
     console.log('server running on port 3000');
-    
 })
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{console.log('Database connected');})
+.catch((error)=>{console.log(error);})
