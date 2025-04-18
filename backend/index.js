0c99e1f8fc69be1c05bbc1231a884d057aa2f5d0
Subhin MS <subhinsubramanian@gmail.com>
2025-04-18 11:32:27 +0530

    Created and tested a sample api

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
 dotenv.config();
 
+// port settings
 app.listen(3000,()=>{
     console.log('server running on port 3000');
 })
 
+// Connecting to mongodb database
 mongoose.connect(process.env.MONGO)
 .then(()=>{console.log('Database connected');})
 .catch((error)=>{console.log(error);})
+
+// Api endpoints
+app.use('/api/user',userRouter);
